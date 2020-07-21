bdfd0adb21f0905ed7b4c06e0ae45b66b705738e
bartek <bartosz.chmiel@saint-gobain.com>
2020-07-21 11:04:52 +0200

    Modyfikacja z templatem posts.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(Data_Publikacji__lte=timezone.now()).order_by('Data_Publikacji')
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
 
 # Create your views here.
