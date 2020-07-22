6428fe707e66dc005da120d30854b581ea91ab5c
bartek <bartosz.chmiel@saint-gobain.com>
2020-07-22 08:24:18 +0200

    Ending zmian w stronce edit i create

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 from django.shortcuts import render, get_object_or_404
+
+
 def post_list(request):
     posts = Post.objects.filter(Data_Publikacji__lte=timezone.now()).order_by('Data_Publikacji')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
-# Create your views here.
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.Autor = request.user
+            post.Data_Publikacji = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.Autor = request.user
+            post.Data_Publikacji = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
